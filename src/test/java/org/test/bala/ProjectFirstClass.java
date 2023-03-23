ca7f39158ab10517964b28f7c2874975b561d096
balasudhan23 <balasudhan2000@gmail.com>
2023-03-23 21:30:28 +0530

    Task-1

---
+++ src/test/java/org/test/bala/ProjectFirstClass.java
@@ -0,0 +1,10 @@
+package org.test.bala;
+
+import org.test.baseclass.Bala_BaseClass;
+
+public class ProjectFirstClass extends Bala_BaseClass {
+	public static void main(String[] args) {
+		chromeLaunch();
+	}
+
+}
